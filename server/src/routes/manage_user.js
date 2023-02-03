bb020a76430363b541c9455c825d5af9f9d81369
DESKTOP-GONJQ2E\User <viranprabathjayathilake@gmail.com>
2023-02-03 15:11:27 +0530

    Add Post method to manage user model

--- server/src/routes/manage_user.js
+++ server/src/routes/manage_user.js
@@ -24,6 +24,7 @@ manage_user_Router.post("/", async (req, res) => {
   }
 });
 
+
 // Get user details by Email
 manage_user_Router.get("/email/:email", async (req, res) => {
   try {
@@ -40,6 +41,31 @@ manage_user_Router.get("/email/:email", async (req, res) => {
     }
 
     res.status(200).send(user.userId);
+  } catch (err) {
+    return res.status(500).send(`Error: ${err.message}`);
+  }
+});
+
+// Get user details by email & password
+manage_user_Router.post("/userdata", async (req, res) => {
+  try {
+    let user = await userModel.findOne({
+      email: req.body.email,
+    });
+    if (!user) {
+      let errorObj = {
+        message: "The given email address does not match in our system",
+        statusCode: "NOT FOUND",
+      };
+
+      return res.status(404).send(errorObj);
+    }
+
+    let pwValid = await bcrypt.compare(req.body.password, user.password);
+
+    if (!pwValid) return res.status(400).send("Password Incorrect");
+
+    return res.status(200).send(user.userId);
   } catch (err) {
     return res.status(500).send(`Error: ${err.message}`);
   }
