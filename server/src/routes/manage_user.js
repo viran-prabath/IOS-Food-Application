9ed8958d733b806ce79328a85406910078b93346
DESKTOP-GONJQ2E\User <viranprabathjayathilake@gmail.com>
2023-02-04 00:30:19 +0530

    change manage user route

--- server/src/routes/manage_user.js
+++ server/src/routes/manage_user.js
@@ -25,8 +25,8 @@ manage_user_Router.post("/", async (req, res) => {
 });
 
 
-// Get user details by Email
-manage_user_Router.get("/email/:email", async (req, res) => {
+// Get user details by email & password
+manage_user_Router.get("/userdata/email=:email&password=:password", async (req, res) => {
   try {
     let user = await userModel.findOne({
       email: req.params.email,
@@ -40,36 +40,40 @@ manage_user_Router.get("/email/:email", async (req, res) => {
       return res.status(404).send(errorObj);
     }
 
+    let pwValid = await bcrypt.compare(req.params.password, user.password);
+
+    if (!pwValid) return res.status(400).send("Password Incorrect");
+
     res.status(200).send(user.userId);
   } catch (err) {
     return res.status(500).send(`Error: ${err.message}`);
   }
 });
 
-// Get user details by email & password
-manage_user_Router.post("/userdata", async (req, res) => {
-  try {
-    let user = await userModel.findOne({
-      email: req.body.email,
-    });
-    if (!user) {
-      let errorObj = {
-        message: "The given email address does not match in our system",
-        statusCode: "NOT FOUND",
-      };
+// // Get user details by email & password
+// manage_user_Router.post("/userdata", async (req, res) => {
+//   try {
+//     let user = await userModel.findOne({
+//       email: req.body.email,
+//     });
+//     if (!user) {
+//       let errorObj = {
+//         message: "The given email address does not match in our system",
+//         statusCode: "NOT FOUND",
+//       };
 
-      return res.status(404).send(errorObj);
-    }
+//       return res.status(404).send(errorObj);
+//     }
 
-    let pwValid = await bcrypt.compare(req.body.password, user.password);
+//     let pwValid = await bcrypt.compare(req.body.password, user.password);
 
-    if (!pwValid) return res.status(400).send("Password Incorrect");
+//     if (!pwValid) return res.status(400).send("Password Incorrect");
 
-    return res.status(200).send(user.userId);
-  } catch (err) {
-    return res.status(500).send(`Error: ${err.message}`);
-  }
-});
+//     return res.status(200).send(user.userId);
+//   } catch (err) {
+//     return res.status(500).send(`Error: ${err.message}`);
+//   }
+// });
 
 
 module.exports = manage_user_Router;
