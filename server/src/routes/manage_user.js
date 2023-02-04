be81ed1ac620d258dff8c801f13abaafd1ceb272
DESKTOP-GONJQ2E\User <viranprabathjayathilake@gmail.com>
2023-02-04 12:05:24 +0530

    Update manage user model

--- server/src/routes/manage_user.js
+++ server/src/routes/manage_user.js
@@ -25,54 +25,54 @@ manage_user_Router.post("/", async (req, res) => {
 });
 
 
-// Get user details by email & password
-manage_user_Router.get("/userdata/email=:email&password=:password", async (req, res) => {
-  try {
-    let user = await userModel.findOne({
-      email: req.params.email,
-    });
-    if (!user) {
-      let errorObj = {
-        message: "not found"
-      };
-
-      return res.status(404).send(errorObj);
-    }
-
-    let pwValid = await bcrypt.compare(req.params.password, user.password);
-
-    if (!pwValid) return res.status(400).send("Password Incorrect");
-
-    res.status(200).send(user.userId);
-  } catch (err) {
-    return res.status(500).send(`Error: ${err.message}`);
-  }
-});
-
 // // Get user details by email & password
-// manage_user_Router.post("/userdata", async (req, res) => {
+// manage_user_Router.get("/userdata/email=:email&password=:password", async (req, res) => {
 //   try {
 //     let user = await userModel.findOne({
-//       email: req.body.email,
+//       email: req.params.email,
 //     });
 //     if (!user) {
 //       let errorObj = {
-//         message: "The given email address does not match in our system",
-//         statusCode: "NOT FOUND",
+//         message: "not found"
 //       };
 
 //       return res.status(404).send(errorObj);
 //     }
 
-//     let pwValid = await bcrypt.compare(req.body.password, user.password);
+//     let pwValid = await bcrypt.compare(req.params.password, user.password);
 
 //     if (!pwValid) return res.status(400).send("Password Incorrect");
 
-//     return res.status(200).send(user.userId);
+//     res.status(200).send(user.userId);
 //   } catch (err) {
 //     return res.status(500).send(`Error: ${err.message}`);
 //   }
 // });
+
+// Get user details by email & password
+manage_user_Router.post("/userdata", async (req, res) => {
+  try {
+    let user = await userModel.findOne({
+      email: req.body.email,
+    });
+    if (!user) {
+      let errorObj = {
+        message: "The given email address does not match in our system",
+        statusCode: "NOT FOUND",
+      };
+
+      return res.status(404).send(errorObj);
+    }
+
+    let pwValid = await bcrypt.compare(req.body.password, user.password);
+
+    if (!pwValid) return res.status(400).send("Password Incorrect");
+
+    return res.status(200).send(user.userId);
+  } catch (err) {
+    return res.status(500).send(`Error: ${err.message}`);
+  }
+});
 
 // try {
 //   let user = await userModel.findOne({
